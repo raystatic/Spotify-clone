4fbab10bf468aca036ea956636e644439892876b
raystatic <rahul9650ray@gmail.com>
2019-10-09 01:07:52 +0530

    permission setup and songs fetched from device storage

--- app/src/main/java/com/spotify/cl/MainActivity.java
+++ app/src/main/java/com/spotify/cl/MainActivity.java
@@ -1,14 +1,101 @@
 package com.spotify.cl;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_REQUEST = 1;
+
+    ArrayList<String> arraySongList, arrayArtistList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        arraySongList = new ArrayList<>();
+        arrayArtistList = new ArrayList<>();
+
+        checkPermission();
+
+    }
+
+    public void checkPermission(){
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)){
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST);
+            }else{
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST);
+            }
+        }else{
+            Toast.makeText(MainActivity.this,"Please provide permissions from settings",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void getMusic(){
+        ContentResolver contentResolver = getContentResolver();
+        Uri songUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Cursor songCursor = contentResolver.query(songUri,null,null, null, null);
+
+        if (songCursor != null && songCursor.moveToFirst()){
+
+            int songTitle = songCursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
+            int songArtist = songCursor.getColumnIndex(MediaStore.Audio.Media.ARTIST);
+
+            do{
+                String currentTitle = songCursor.getString(songTitle);
+                String currentArtist = songCursor.getString(songArtist);
+                arraySongList.add(currentTitle);
+                arrayArtistList.add(currentArtist);
+            }while (songCursor.moveToNext());
+
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case PERMISSION_REQUEST:{
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    if (ContextCompat.checkSelfPermission(MainActivity.this,
+                            Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                        Toast.makeText(MainActivity.this,"Permission Granted", Toast.LENGTH_SHORT).show();
+
+                        fetchSongs();
+
+                    }
+                }else{
+                    Toast.makeText(MainActivity.this,"Permission Not Granted",Toast.LENGTH_SHORT).show();
+                    //checkPermission();
+                  //  finish();
+                }
+                return;
+            }
+        }
+    }
+
+    private void fetchSongs() {
+        getMusic();
+        Log.d("songlist",arraySongList.size() + arrayArtistList.size()+"");
     }
 }
